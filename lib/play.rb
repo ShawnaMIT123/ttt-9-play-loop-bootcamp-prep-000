1e278b7dcde0d9244e776d9917c4644d30388b5a
Shawna Davis <shawna_davis@alum.mit.edu>
2018-06-30 22:57:56 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,20 +38,9 @@ end
 # Define your play method below
 def play(board)
   counter = 0
-  loop do 
+ while counter < 20
+  puts "The current number is less than 20."
   counter += 1
-  if counter >= 10 
-    break
-  end
 end
 end
-
-counter = 0
- 
-loop do 
-  counter += 1
-  puts "Iteration #{counter} of the loop"
-  if counter >= 10 
-    break
-  end
 end
